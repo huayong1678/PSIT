b3d67eb790c8da3997dc742690badc244916a9d9
huayong1678 <43027964+huayong1678@users.noreply.github.com>
2018-12-19 14:17:06 +0700

    API Test

---
+++ Web/glint/js/api.js
@@ -0,0 +1,3 @@
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=NKE&apikey=QKF958ZJZ1OSBLRQ#Time%20Series%20(Daily)', true);
